ce10cb981c12e434ee4fe14a1d6455cca3e84a8d
andrewcircelli <andrewcircelli@gmail.com>
2020-09-08 12:42:54 -0400

    functional password generator with validation. although must refresh page to choose different length password

--- script.js
+++ script.js
@@ -7,27 +7,19 @@ generateBtn.addEventListener("click", writePassword);
 // Variable Declaration
 let password       = "";
 let passwordLength = "";
-// let useLower       = false;
-// let useUpper       = false;
-// let useSpecial     = false;
-// let useNumber      = false;
 
 // Arrays
-// let alphaLower  = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// let alphaUpper  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// let specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-// let number      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let composition = [alphaLower, alphaUpper, specialChar, number];
+let alphaLower  = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let alphaUpper  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+let number      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// Object Literal
-let options = {
-  alphaLower:  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  alphaUpper:  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  specialChar: ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"],
-  number:      ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-}
-
-
+let composition = [
+  {name:  alphaLower,  include: false},
+  {name:  alphaUpper,  include: false},
+  {name:  specialChar, include: false},
+  {name:  number,      include: false}
+]
 
 function getPasswordLength() {
   while ((isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128)) {
@@ -48,43 +40,42 @@ function getPasswordLength() {
   }
 }
 
-console.log(composition);
+// Assign boolean values to keys on composition array (of objects)
 function getPasswordComposition() {
-  composition[0] = confirm("Would you like to include lower-case characters in your password?");
-  composition[1] = confirm("Would you like to include upper-case characters in your password?");
-  composition[2] = confirm("Would you like to include special characters in your password");
-  composition[3] = confirm("Would you like to include numbers in your password");
+  composition[0].include  = confirm("If possible, would you like to include lower-case characters in your password?");
+  composition[1].include  = confirm("If possible, would you like to include upper-case characters in your password?");
+  composition[2].include  = confirm("If possible, would you like to include special characters in your password");
+  composition[3].include  = confirm("If possible, would you like to include numbers in your password");
 }
+
+// Filters the array of objects who's value "include" is equal to true
 function setPasswordComposition() {
-  composition = composition.filter(function(selection) {
-    if (selection === true) {
-      return selection;
-    }
-  }
-)};
+  composition = composition.filter(function(el) {
+    return el.include === true; 
+  })
+}
 
-
-
-// function generatePassword() {
-//   password = "";
-//   for (let i = 0; i < passwordLength; i++) {
-//     let random_index = Math.floor(Math.random() * alphaLower.length);
-//     password += alphaLower[random_index];
-//   }
-//   for (let j = 0; j <  passwordNumbers; j++) {
-//     let random_index = Math.floor(Math.random() * number.length);
-//     password += number[random_index];
-//   }
-//   for (let y = 0; y <  passwordCharacters; y++) {
-//     let random_index = Math.floor(Math.random() * specialChar.length);
-//     password += specialChar[random_index];
-//   }
-//   return password
-// }
-
+function generatePassword() {
+ password = "";
+ for (let i = 0; i < passwordLength; i++) {
+  //  select random index value based on the length of the filtered composition array
+   let random_index = Math.floor(Math.random() * composition.length);
+  //  select name of array based on the object's "name" key
+   let random_array = composition[random_index].name;
+   password += random_array[Math.floor(Math.random() * random_array.length)]
+   
+ }
+  return password 
+}
 
 // Write password to the #password input
 function writePassword() {
+  composition = [
+    {name:  alphaLower,  include: false},
+    {name:  alphaUpper,  include: false},
+    {name:  specialChar, include: false},
+    {name:  number,      include: false}
+  ]
   getPasswordLength();
   getPasswordComposition();
   setPasswordComposition();
@@ -92,3 +83,10 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
+
+
+// copy and paste button
+// refreshing page as part of writePassword Function
+// what happens when confirm prompts are all ignored... must select one criteria 
+// add comments 
+// screencastify 
